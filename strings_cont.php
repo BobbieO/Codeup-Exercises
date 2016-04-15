a2fdeda35a73b48255971137096a007288fbb888
BobbieO <bobbie2504@msn.com>
2016-04-15 09:26:52 -0500

    Initial commit of strings exercise

---
+++ strings_cont.php
@@ -0,0 +1,20 @@
+<?php
+
+$wall = "We don't need no education \n We don't need no thought control" . PHP_EOL;
+
+echo $wall;
+
+$play = "All the world's a stage,\nAnd all the men and women merely players:\nThey have thier exits and thier entrances;" . PHP_EOL;
+
+echo $play;
+
+$firstName = 'Joe';
+$lastName = 'Blow';
+$address = '123 Any Street';
+
+echo "$lastName, $firstName, $address" . PHP_EOL;
+
+echo "$firstName$lastName $address" . PHP_EOL;
+
+echo "$firstName $lastName\n$address" . PHP_EOL;
+
