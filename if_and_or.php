a2536357a725e5e3a5bd5cdb850f34f2f1e32939
BobbieO <bobbie2504@msn.com>
2016-04-15 10:45:39 -0500

    Step one complete

--- if_and_or.php
+++ if_and_or.php
@@ -7,6 +7,10 @@ $z = 10;
 // TODO:
 // If $x < $y < $z then echo "{$x} < {$y} < {$z}\n";
 
+if (($x < $y) && ($y < $z)) {
+    echo "{$x} < {$y} < {$z}\n";
+}
+
 // TODO:
 // If 0 is less than $x OR $x is less than 10
 // then echo the result as a sentence "0 is less than {$x} OR {$x} is less than 10".
