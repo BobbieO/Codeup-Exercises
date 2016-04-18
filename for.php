b9c9c5991cf408bb0a3beb5c553fee44334413a1
BobbieO <bobbie2504@msn.com>
2016-04-18 13:25:12 -0500

    Added numeric check for user input at start and end point, and error message.

--- for.php
+++ for.php
@@ -4,9 +4,19 @@
 fwrite(STDOUT, "Give me a number to start at:\n");
 $userStart = trim(fgets(STDIN));
 
+// check for numeric start input, give error msg
+if( !is_numeric($userStart) ) {
+    die ("Please enter only numbers.\n");
+}
+
 // ask user for ending number
 fwrite(STDOUT, "Where should we end?\n");
 $userEnd = trim(fgets(STDIN));
+
+// check for numeric end input, give error msg
+if( !is_numeric($userEnd) ) {
+    die ("Please enter only numbers.\n");
+}
 
 // ask user for incrementing number
 fwrite(STDOUT, "What should we increment by?\n");
@@ -14,6 +24,7 @@ $userIncrement = trim(fgets(STDIN));
 
 // display numbers from start to finish using for-loop, with desired increment
 echo "Our numbers are {$userStart} to {$userEnd}\n";
+
 for ($i = $userStart; $i <= $userEnd; $i +=$userIncrement) {
     echo "{$i}\n";
 
@@ -22,8 +33,6 @@ for ($i = $userStart; $i <= $userEnd; $i +=$userIncrement) {
         $i += 1;
     }
 }
-
-// error msg if not numberic or blank
 
 
 
