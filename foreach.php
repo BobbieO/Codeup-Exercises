08c4bb175f3d6ec64c50a439a45d971af21d6a8d
BobbieO <bobbie2504@msn.com>
2016-04-18 15:15:22 -0500

    Initial commit of exercise. Completed foreach loop that outputs type of array item.

---
+++ foreach.php
@@ -0,0 +1,40 @@
+<?php
+
+$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
+
+// loop that outputs each item's type
+foreach($things as $thing) {
+    if (is_int($thing)) {
+        echo "integer\n";
+    } elseif (is_float($thing)) {
+        echo "float\n";
+    } elseif (is_bool($thing)) {
+        echo "boolean\n";
+    } elseif (is_array($thing)) {
+        echo "array\n";
+    } elseif (is_null($thing)) {
+        echo "null\n";
+    } elseif (is_string($thing)) {
+        echo "string\n";
+    }
+}
+
+
+
+
+//integer, float, boolean, array, null, string
+
+
+// example: For vs foreach:
+// $names [Lorena, Niki, Pancho, Will]
+
+// for ($i = 0;  $i < count($names); $i++) {
+//     $studentName = $names[$i];
+//     echo "Hey everybody, say hello to $studentName\n";
+//     echo "Hi $studentName!\n";
+// }
+
+// foreach($names as $studentName) {
+//     echo "Hey everybody, say hello to $studentName\n";
+//     echo "Hi $studentName!\n";
+// }
