d149ed1d2d877b5c31295336342eb54cc2cbe34d
BobbieO <bobbie2504@msn.com>
2016-04-18 13:55:46 -0500

    Added check for number being both a multiple of 3 and five. Completed exercise.

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,19 +1,25 @@
 <?php
 
-
-// print/echo numbers 1 to 100
+// print numbers 1 to 100
 for ($i = 0; $i <= 100; $i++) {
 
+    //print FIZZBUZZ for numbers which are both 3-5 mults
+    if ( ($i % 3 == 0) && ($i % 5 == 0) ) {
+        echo "FizzBuzz\n";
+
     //print FIZZ for numbers which are multiples of three
-       if ($i % 3 == 0) {
-            echo "Fizz\n";
-        } else {
-            echo "{$i}\n";
-        }
+    } elseif ($i % 3 == 0) {
+        echo "Fizz\n";
+
+    //print BUZZ for numbers which are multiples of five
+    } elseif ($i % 5 == 0) {
+        echo "Buzz\n";
+
+    // if none of above apply
+    } else {
+        echo "{$i}\n";
+    }
 }
 
 
-//print BUZZ for numbers which are multiples of five
 
-
-//print FIZZBUZZ for numbers which are both 3-5 mults
