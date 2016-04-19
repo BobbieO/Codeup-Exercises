1cdd4a8ab62678aea8fff0f01e0fcc76644fcac5
BobbieO <bobbie2504@msn.com>
2016-04-19 14:31:01 -0500

    Changed positioning of echo statements to cluster at end

--- arithmetic.php
+++ arithmetic.php
@@ -9,34 +9,30 @@ function add($a, $b)
     return $a + $b;
 }
 
-echo add(5, 6) . PHP_EOL;
-
 function subtract($a, $b)
 {
     return $a - $b;
 }
-
-echo subtract(10, 2) . PHP_EOL;
 
 function multiply($a, $b)
 {
     return $a * $b;
 }
 
-echo multiply(4, 6) . PHP_EOL;
-
 function divide($a, $b)
 {
     return $a / $b;
 }
-
-echo divide(66, 2) . PHP_EOL;
 
 function modulus($a, $b)
 {
     return $a % $b;
 }
 
+echo add(5, 6) . PHP_EOL;
+echo subtract(10, 2) . PHP_EOL;
+echo multiply(4, 6) . PHP_EOL;
+echo divide(66, 2) . PHP_EOL;
 echo modulus(3, 2) . PHP_EOL;
 
 
