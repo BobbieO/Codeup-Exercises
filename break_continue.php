b3a19e3e94462f4224b656ed7aa3ef013610290c
BobbieO <bobbie2504@msn.com>
2016-04-19 11:10:25 -0500

    Added break loop

--- break_continue.php
+++ break_continue.php
@@ -1,10 +1,20 @@
 <?php
 
 for ($i = 0; $i <= 100; $i++) {
-    
+
     if ($i % 2 !== 0) {
         continue;
     }
     
     echo "{$i}\n";
+}
+
+
+
+for ($i = 0; $i <= 100; $i++) {
+    echo "{$i}\n";
+
+    if ($i == 10) {
+        break;
+    }
 }
