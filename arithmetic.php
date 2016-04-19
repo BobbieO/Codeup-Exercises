cd6b79f36c20fc7d2b95803363e979f2f584295f
BobbieO <bobbie2504@msn.com>
2016-04-19 15:38:04 -0500

    Added divide by zero check and message

--- arithmetic.php
+++ arithmetic.php
@@ -29,7 +29,7 @@ function multiply($a, $b)
 
 function divide($a, $b)
 {
-    if (is_numeric($a) && is_numeric($b)) { 
+    if ( (is_numeric($a) && is_numeric($b)) && ($b != 0) ) { 
         return $a / $b;
     } else {
         return "Error: {$a} and {$b}. \$a and \$b must both be numbers.\n"; 
@@ -48,7 +48,7 @@ function modulus($a, $b)
 echo add(5, 6) . PHP_EOL;
 echo subtract(10, 2) . PHP_EOL;
 echo multiply(4, 6) . PHP_EOL;
-echo divide(66, 2) . PHP_EOL;
+echo divide(66, 0) . PHP_EOL;
 echo modulus(3, 2) . PHP_EOL;
 
 
