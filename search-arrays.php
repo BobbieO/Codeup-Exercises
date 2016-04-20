706e5c280f14440acbeed8b78a6fd51ab872b047
BobbieO <bobbie2504@msn.com>
2016-04-20 14:04:48 -0500

    Added function to return name. Added function to return true/false if search returns positive

--- search-arrays.php
+++ search-arrays.php
@@ -3,3 +3,16 @@
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+$firstName = 'Tina';
+$search = array_search($firstName, $names);
+
+if($search!==false) {
+    echo $names[$search] . PHP_EOL;
+}
+
+if($search==$firstName) {
+    echo "TRUE\n";
+} else {
+    echo "FALSE\n";
+}
