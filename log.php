b3581eb2062033d4bd47b3c771926db6d2584174
BobbieO <bobbie2504@msn.com>
2016-05-02 10:20:26 -0500

    created Log class, logMessage function, info function and error function.

--- log.php
+++ log.php
@@ -1,49 +1,60 @@
 <?php
 
-
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Classes and Objects I</title>
-</head>
-<body>
-
-</body>
-</html>
-
-
-<!-- NOTES
-
-//this is the class (the blueprint, the plan)
-class Person
+class Log
 {
-    public $firstName;
-    public $lastName;
-    public $fruit = array();
+    public $filename;
 
-    public function roamCountryside()
+    public function logMessage($logLevel, $message)
     {
-        $distance = mt_rand(1, 50);
-        return $this->firstName . " is walking [$distance} miles west.";
+        $handle = fopen($this->filename, 'a');
+        fwrite($handle, PHP_EOL . "[" . $logLevel . "]" . $message);
+        fclose($handle);
+    }
+
+    public function info($message) 
+    {
+        logMessage("INFO", $message);
+    }
+
+    public function error($message)
+    {
+        logMessage("ERROR", $message);
     }
 }
 
-//instantiated class ($person is the object, instance of plan)
-$person = new Person;
-$person->firstName = 'Isaac';
-$person->lastName = 'Castillo';
-$person->fruit = array('apple', 'orange'); 
-
-//another object of person in the class Person
-$person2 = new Person;
-$person2->firstName = 'Bobbie';
-
-echo $person->roamCountryside();
-echo $person2->roamCountryside();
+?>
 
 
+// <!-- NOTES
+
+// //this is the class (the blueprint, the plan)
+// class Person
+// {
+//     public $firstName;
+//     public $lastName;
+//     public $fruit = array();
+
+//     public function roamCountryside()
+//     {
+//         $distance = mt_rand(1, 50);
+//         return $this->firstName . " is walking [$distance} miles west.";
+//     }
+// }
+
+// //instantiated class ($person is the object, instance of plan). This would be in a separate file in actual use, not here.
+// $person = new Person;
+// $person->firstName = 'Isaac';
+// $person->lastName = 'Castillo';
+// $person->fruit = array('apple', 'orange'); 
+
+// //another object of person in the class Person
+// $person2 = new Person;
+// $person2->firstName = 'Bobbie';
+
+// echo $person->roamCountryside();
+// echo $person2->roamCountryside();
 
 
--->
+
+
+// -->
