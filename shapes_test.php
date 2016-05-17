a53e3982fd0cddd142bfe79dd93699ecbf5848ca
BobbieO <bobbie2504@msn.com>
2016-05-17 10:05:08 -0500

    Edited variable capitalization

--- shapes_test.php
+++ shapes_test.php
@@ -3,17 +3,17 @@
 REQUIRE_ONCE 'rectangle.php';
 REQUIRE_ONCE 'square.php';
 
-$Rectangle = new Rectangle(20, 40);
+$rectangle = new Rectangle(20, 40);
 echo 'Rectangle One area is: ' . $Rectangle->area() . PHP_EOL;
 
-$Rectangle = new Rectangle(3, 100);
+$rectangle = new Rectangle(3, 100);
 echo 'Rectangle Two area is: ' . $Rectangle->area() . PHP_EOL;
 
-$Square = new Square(2, 2);
+$square = new Square(2, 2);
 echo 'Square area is: ' . $Square->area() . PHP_EOL;
 echo 'Square perimeter is: ' . $Square->perimeter() . PHP_EOL;
 
-$Square = new Square(8, 8);
+$square = new Square(8, 8);
 echo 'Square area is: ' . $Square->area() . PHP_EOL;
 echo 'Square perimeter is: ' . $Square->perimeter() . PHP_EOL;
 
