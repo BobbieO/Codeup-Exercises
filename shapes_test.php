5abaf99e510bdb0a9da7d0495f6d0ffdce40479d
BobbieO <bobbie2504@msn.com>
2016-05-17 11:19:15 -0500

    Edited variable names

--- shapes_test.php
+++ shapes_test.php
@@ -4,18 +4,18 @@ REQUIRE_ONCE 'rectangle.php';
 REQUIRE_ONCE 'square.php';
 
 $rectangle = new Rectangle(20, 4);
-echo 'Rectangle One area is: ' . $Rectangle->area() . PHP_EOL;
+echo 'Rectangle One area is: ' . $rectangle->area() . PHP_EOL;
 
 $rectangle = new Rectangle(3, 10);
-echo 'Rectangle Two area is: ' . $Rectangle->area() . PHP_EOL;
+echo 'Rectangle Two area is: ' . $rectangle->area() . PHP_EOL;
 
 $square = new Square(4);
-echo 'Square area is: ' . $Square->area() . PHP_EOL;
+echo 'Square area is: ' . $square->area() . PHP_EOL;
 echo 'Square perimeter is: ' . $Square->perimeter() . PHP_EOL;
 
 $square = new Square(9);
-echo 'Square area is: ' . $Square->area() . PHP_EOL;
-echo 'Square perimeter is: ' . $Square->perimeter() . PHP_EOL;
+echo 'Square area is: ' . $square->area() . PHP_EOL;
+echo 'Square perimeter is: ' . $square->perimeter() . PHP_EOL;
 
 
 ?>
