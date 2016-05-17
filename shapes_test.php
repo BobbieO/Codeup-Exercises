2fe4b69d6be104a5f9dedb64a05eed52a2edc35e
BobbieO <bobbie2504@msn.com>
2016-05-17 09:37:24 -0500

    Added square test cases

--- shapes_test.php
+++ shapes_test.php
@@ -3,10 +3,19 @@
 REQUIRE_ONCE 'rectangle.php';
 
 $Rectangle = new Rectangle(20, 40);
-echo $Rectangle->area() . PHP_EOL;
+echo 'Rectangle One area is: ' . $Rectangle->area() . PHP_EOL;
 
 $Rectangle = new Rectangle(3, 100);
-echo $Rectangle->area() . PHP_EOL;
+echo 'Rectangle Two area is: ' . $Rectangle->area() . PHP_EOL;
+
+$Square = new Square(2, 2);
+echo 'Square area is: ' . $Square->area() . PHP_EOL;
+echo 'Square perimeter is: ' . $Square->perimeter() . PHP_EOL;
+
+$Square = new Square(8, 8);
+echo 'Square area is: ' . $Square->area() . PHP_EOL;
+echo 'Square perimeter is: ' . $Square->perimeter() . PHP_EOL;
+
 
 ?>
 
--- square.php
+++ square.php
@@ -2,6 +2,13 @@
 
 REQUIRE 'rectangle.php';
 
+class Square extends Rectangle
+{
+    public function perimeter() {
+        return ($this->height * 2) + ($this->width * 2);
+    }
+
+};
 
 
 
