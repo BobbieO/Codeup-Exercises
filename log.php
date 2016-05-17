5df9eb5c9c284d20a53a81c248f0bdf626015013
BobbieO <bobbie2504@msn.com>
2016-05-17 14:08:51 -0500

    Made filename and handler protected; added setter.

--- log.php
+++ log.php
@@ -5,16 +5,25 @@ date_default_timezone_set("America/Chicago");
 
 class Log
 {
-    public $filename;
-    public $handle;
+    protected $filename;
+    protected $handle;
    
     public function __construct($prefix = 'log')
     {
         $currentDate = date('Y-m-d');
        
-        $this->filename = "{$prefix}-{$currentDate}.log";
+        $this->setFilename("{$prefix}-{$currentDate}.log");
 
         $this->handle = fopen($this->filename, 'a');
+    }
+
+    public function setFilename($filename)
+    {
+        if(is_string($filename)) {
+            $this->filename = $filename;
+        } else {
+            echo "Not a string.";
+        }
     }
     
     public function logMessage($logLevel, $message)
